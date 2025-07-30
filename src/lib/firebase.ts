f6ceebc67343d4a516a26f8f59cebf6d85b53738
Aadarsh Saini <aadarshsaini739@gmail.com>
2025-07-30 17:45:21 +0000

    // Import the functions you need from the SDKs you need import { initial

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3diPhDNOTyCHJ1gsmNXT_juFypkn92aU",
+  authDomain: "ecstatic-mhefl.firebaseapp.com",
+  projectId: "ecstatic-mhefl",
+  storageBucket: "ecstatic-mhefl.appspot.com",
+  messagingSenderId: "698831772358",
+  appId: "1:698831772358:web:04737379ac9ee148fcd5c7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
