1385581d371027511704a760e631cd1dc840260b
Aadarsh Saini <aadarshsaini739@gmail.com>
2025-09-14 11:26:07 +0000

    Maine QR ko scan Kiya or URL se bhi home screen per add kiya Chrome se t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
       </head>
       <body className="font-body antialiased">
         <ClientLayout>{children}</ClientLayout>
