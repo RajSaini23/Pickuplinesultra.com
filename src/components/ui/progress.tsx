b66d3fd145c31559c11833666c086d8c021ebf73
Aadarsh Saini <aadarshsaini739@gmail.com>
2025-10-12 15:03:06 +0000

    Abhi FIR tumne change nahin kiya change karo fire color Aage rahega Jo f

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -18,7 +19,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-gradient-to-r from-yellow-400 via-orange-500 to-red-500 transition-all"
+      className="h-full w-full flex-1 bg-gradient-to-l from-red-500 via-orange-500 to-yellow-400 transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
@@ -26,3 +27,4 @@ const Progress = React.forwardRef<
 Progress.displayName = ProgressPrimitive.Root.displayName
 
 export { Progress }
+
